427898fa6bb8a20dd75d5d40abb8873ef5a76b74
Smijo-Saji <Smijo-Saji>
2024-04-09 17:00:32 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [uname, setState] = useState(0);
+
+  function increment() {
+    setState(uname + 1);
+  }
+  function decrement() {
+    if (uname > 0) {
+      setState(uname - 1);
+    }
+  }
+  function reset() {
+    setState(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex justify-content-center align-items-center">
+      <div className="p-4 text-center main-div  shadow rounded">
+        <h1 className="text-center mt-3 mb-5">Counter-App</h1>
+        <h1 className="mb-5">{uname}</h1>
+        <div className="d-flex justify-content-evenly">
+          <div>
+            <button onClick={increment} className="bg-success text-white">
+              +
+            </button>
+          </div>
+          <div>
+            <button onClick={reset} className="bg-dark text-white">
+              Reset
+            </button>
+          </div>
+          <div>
+            <button onClick={decrement} className="bg-danger text-white">
+              -
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
